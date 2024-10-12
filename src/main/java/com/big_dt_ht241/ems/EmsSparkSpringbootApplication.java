1b7c10b7ab7086fbbc694dd73df6e383312b1b95
TungVo <tonyvohcmut@gmail.com>
2024-10-12 19:53:25 +0700

    init project

---
+++ src/main/java/com/big_dt_ht241/ems/EmsSparkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.big_dt_ht241.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsSparkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsSparkSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/big_dt_ht241/ems/EmsSparkSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big_dt_ht241.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsSparkSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
